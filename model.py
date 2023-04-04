f53c2b559a88cdd2f9e8db422005590e5f1f66f5
agamemnonc <agamemnon.krasoulis@gmail.com>
2023-04-04 17:14:17 +0300

    Training runs smoothly

--- model.py
+++ model.py
@@ -6,52 +6,48 @@ from torchmetrics import Accuracy, MatthewsCorrCoef
 
 
 class ColaModule(LightningModule):
-    def __init__(self, model: nn.Module, optimizer: Optimizer):
+    def __init__(self, model: nn.Module, head: nn.Module, optimizer: Optimizer):
         """
         Args:
-            model: PyTorch model
+            model: PyTorch model for feature extraction
+            head: PyTorch model for classification
+            optimizer: PyTorch optimizer
         """
         super().__init__()
         self.model = model
+        self.head = head
         self.optimizer = optimizer
 
         # Metrics
-        self.train_accuracy = Accuracy(task='binary')
-        self.valid_accuracy = Accuracy(task='binary')
+        self.train_accuracy = Accuracy(task="binary")
+        self.valid_accuracy = Accuracy(task="binary")
 
         self.save_hyperparameters()
 
-    def forward(self, batch, batch_idx):
-        return self.model(batch['x'])
+    def forward(self, input_ids, attention_mask):
+        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
+        # This accesses embeddieng of the CLS token output from the last layer
+        h_cls = outputs.last_hidden_state[:, 0, :]
+        # We pass it through the classification head to get the logits
+        logits = self.head(h_cls)
+        preds = logits.argmax(dim=-1)
+        return logits, preds
 
     def training_step(self, batch, batch_idx):
-        logits  = self.forward(batch, batch_idx)
-        loss = F.cross_entropy(logits, batch['label'])
-        self.log(
-            "train_loss",
-            loss,
-            prog_bar=True,
-            on_step=True,
-            on_epoch=True)
+        logits, preds = self.forward(batch["input_ids"], batch["attention_mask"])
+        loss = F.cross_entropy(logits, batch["label"])
+        self.log("train_loss", loss, prog_bar=True, on_step=True, on_epoch=True)
+        train_acc = self.train_accuracy(preds, batch["label"])
+        self.log("train/acc", train_acc, prog_bar=True, on_step=False, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        logits = self.forward(batch, batch_idx)
-        loss = F.cross_entropy(logits, batch['label'])
-        self.log(
-            "val_loss",
-            loss,
-            prog_bar=True,
-            on_step=False,
-            on_epoch=True)
-        accuracy = 2.0
-        self.log(
-            "val_acc",
-            accuracy,
-            prog_bar=True,
-            on_step=False,
-            on_epoch=True
-        )
+        logits, preds = self.forward(batch["input_ids"], batch["attention_mask"])
+        loss = F.cross_entropy(logits, batch["label"])
+        self.log("valid/loss", loss, prog_bar=True, on_step=False, on_epoch=True)
+
+        valid_acc = self.valid_accuracy(preds, batch["label"])
+        self.log("valid/acc", valid_acc, prog_bar=True, on_step=False, on_epoch=True)
 
     def test_step(self, *args, **kwargs):
         pass
@@ -61,4 +57,3 @@ class ColaModule(LightningModule):
 
     def configure_optimizers(self):
         return self.optimizer
-
--- train.py
+++ train.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     batch_size = 32
     max_length = 256
     lr = 3e-4
+    lr_scale = 10  # Learning rate for the base model parameters is lr / lr_scale
     weight_decay = 1e-4
     max_epochs = 10
     max_steps = -1
@@ -19,26 +20,30 @@ if __name__ == "__main__":
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     datamodule = ColaDataModule(
-        tokenizer=tokenizer,
-        batch_size=batch_size,
-        max_length=max_length
+        tokenizer=tokenizer, batch_size=batch_size, max_length=max_length
     )
 
-    # Debug
+    # Main logic
     model = AutoModel.from_pretrained(model_name)
+    head = torch.nn.Linear(model.config.hidden_size, 2)
     optimizer = Adam(
-        params=model.parameters(),
-        lr=lr,
-        weight_decay=weight_decay)
-    colamodule = ColaModule(model=model, optimizer=optimizer)
+        [
+            {
+                "params": model.parameters(),
+                "lr": lr / lr_scale,
+                "weight_decay": weight_decay,
+            },
+            {"params": head.parameters(), "lr": lr, "weight_decay": weight_decay},
+        ]
+    )
+    colamodule = ColaModule(model=model, head=head, optimizer=optimizer)
     trainer = Trainer(
         gpus=(1 if torch.cuda.is_available() else 0),
-        max_epochs=10,
+        max_epochs=max_epochs,
         max_steps=max_steps,
         fast_dev_run=fast_dev_run,
         logger=False,
         enable_checkpointing=False,
-        callbacks=None)
-    trainer.fit(
-        model=colamodule,
-        datamodule=datamodule)
+        callbacks=None,
+    )
+    trainer.fit(model=colamodule, datamodule=datamodule)
